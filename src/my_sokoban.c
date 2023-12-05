96d2abc77479f5289ebb46ec85e6463f5433b9b6
Angel Daniel Sanchez Garcia <angel-daniel.sanchez-garcia@epitech.eu>
2023-12-05 16:13:20 +0100

    FEAT: print help

--- src/my_sokoban.c
+++ src/my_sokoban.c
@@ -20,15 +20,31 @@ static int report_error(char *str)
     return (84);
 }
 
-int main(int argc, char **argv)
+static int print_help(void)
 {
-    board *b;
+    my_putstr("USAGE\n"\
+    "\t./my_sokoban map\n"\
+    "DESCRIPTION\n"\
+    "\tmap file representing the warehouse map, containing ‘#’ for walls,\n"\
+    "\t\t‘P’ for the player, ‘X’ for boxes and ‘O’ for storage locations.\n");
+    return (84);
+}
 
-    if (argc != 2)
-        return (report_error("Invalid amount of parameters.\n"));
-    b = board_from_file(argv[1]);
+static int game(char *path)
+{
+    board *b = board_from_file(path);
     if (b == NULL)
         return (report_error("Error preparing the board\n"));
     destroy_board(b);
     return (0);
+}
+
+int main(int argc, char **argv)
+{
+    if (argc != 2)
+        return (report_error("Invalid amount of parameters.\n"));
+    if (my_strcmp(argv[1], "-h") == 0)
+        return(print_help());
+    else
+        return (game(argv[1]));
 }
